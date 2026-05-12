ab25f2e942c3779b56256d39bdb1703a2c385bc2
karina <karinagazizva4127@gmail.com>
2026-05-12 20:10:31 +0300

    for front

--- app/main.py
+++ app/main.py
@@ -21,10 +21,10 @@ app = FastAPI(
     version="1.0"
 )
 
-# Настройка CORS
+# Разрешаем запросы с фронта
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
